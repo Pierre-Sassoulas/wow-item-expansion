bbd9d3c933a1c4a0c5eac8b3d0eecc4f862007fb
Martin Brajer <martin.brajer@seznam.cz>
2023-03-23 13:46:38 +0100

    Track items by ID instead of link
    Link is poorly defined for mythic keys.

--- ItemExpansion.lua
+++ ItemExpansion.lua
@@ -6,20 +6,20 @@ function ToolTipHook(t, data)
 	if t["GetItem"] == nil then
 		return
 	end
-	local link = select(2, t:GetItem())
+	-- t = {name, link, itemID}
+	local itemID = select(3, t:GetItem())
 
 	-- Item 'data["hyperlink"]' exists (not nil) on the first item load only,
 	-- so no double show happens.
-	if not link or data["hyperlink"] then
+	if not itemID or data["hyperlink"] then
 		return
 	end
 
 	-- Zero indexed to lua standard one indexed (also used by 'EJ_GetTierInfo').
-	local expacID = select(15, GetItemInfo(link)) + 1
+	local expacID = select(15, GetItemInfo(itemID)) + 1
 
 	-- Classic or info missing.
 	if expacID == 1 then
-		local itemID = GetItemInfoFromHyperlink(link)
 		expacID = ItemExpansion_G.exceptions[itemID]
 		if expacID == nil then
 			return
