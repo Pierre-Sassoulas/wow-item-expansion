1faa1a29dd4bc70f7a9d6843102637bf7271c0a5
Martin Brajer <martin.brajer@seznam.cz>
2023-05-07 09:08:07 +0200

    New WoW patch interface and new exceptions

--- exceptions.lua
+++ exceptions.lua
@@ -1,6 +1,13 @@
 
 -- { [itemID] = expansionNumber  -- name }
 ItemExpansion_G.exceptions = {
+    [23821] = 2,  -- Zapthrottle Mote Extractor
     [49040] = 3,  -- Jeeves
+    [80914] = 5,  -- Mourning Glory
+    [86425] = 5,  -- Cooking School Bell
+    [87216] = 5,  -- Thermal Anvil
+    [114943] = 6,  -- Ultimate Gnomish Army Knife
     [128353] = 6,  -- Admiral's Compass
+    [144341] = 7,  -- Rechargeable Reaves Battery
+    [194510] = 10,  -- Iskaaran Harpoon
 }
